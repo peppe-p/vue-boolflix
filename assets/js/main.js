e8a7e46f959617c153c19852e943f599b4bddfbd
peppe-p <ombrachiara96@gmail.com>
2021-06-08 14:40:21 +0200

    fix axios request and html

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,7 @@ const app = new Vue({
     data: {
         api_key: "29ba452e71ed0b0015f2c334d30767e2",
         list_film: "",
+        search_content: "",
     },
 
     methods: {
@@ -10,11 +11,14 @@ const app = new Vue({
     },
 
     mounted() {
-        const url = `https://api.themoviedb.org/3/search/movie?api_key=${this.api_key}&query=ritorno+al+futuro`;
-        axios
-            .get(url)
-            .then(info => {
-                console.log(info.data.results[1]);
-            })
+        const searchBttn = document.getElementById("search");
+        searchBttn.addEventListener("click", () => {
+            const url = `https://api.themoviedb.org/3/search/movie?api_key=${this.api_key}&query=${this.search_content}`;
+            axios
+                .get(url)
+                .then(info => {
+                    console.log(info.data);
+                })
+        });
     }
 });
